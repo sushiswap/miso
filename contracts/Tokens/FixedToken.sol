bf9632cf67a4d7bd25ce57b1e21d95abc963922d
z0r0z <92001561+z0r0z@users.noreply.github.com>
2021-11-02 12:09:28 -0400

    Update FixedToken.sol
    refactor and bump compiler

--- contracts/Tokens/FixedToken.sol
+++ contracts/Tokens/FixedToken.sol
@@ -1,52 +1,48 @@
-pragma solidity 0.6.12;
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity >=0.8.0;
 
 import "./ERC20.sol";
 import "../interfaces/IMisoToken.sol";
 
 // ---------------------------------------------------------------------
-//
 // From the MISO Token Factory
 //
 // Made for Sushi.com 
 // 
 // Enjoy. (c) Chef Gonpachi 2021 
 // <https://github.com/chefgonpachi/MISO/>
-//
-// ---------------------------------------------------------------------
-// SPDX-License-Identifier: GPL-3.0                        
 // ---------------------------------------------------------------------
 
 contract FixedToken is ERC20, IMisoToken {
-
     /// @notice Miso template id for the token factory.
     /// @dev For different token types, this must be incremented.
     uint256 public constant override tokenTemplate = 1;
     
-    /// @dev First set the token variables. This can only be done once
-    function initToken(string memory _name, string memory _symbol, address _owner, uint256 _initialSupply) public  {
-        _initERC20(_name, _symbol);
-        _mint(msg.sender, _initialSupply);
+    /// @dev First set the token variables. This can only be done once.
+    function initToken(
+        string memory _name, 
+        string memory _symbol, 
+        address _owner, 
+        uint256 _initialSupply
+    ) public {
+        initERC20(_name, _symbol);
+        _mint(_owner, _initialSupply);
     }
+    
     function init(bytes calldata _data) external override payable {}
 
-   function initToken(
-        bytes calldata _data
-    ) public override {
-        (string memory _name,
-        string memory _symbol,
-        address _owner,
-        uint256 _initialSupply) = abi.decode(_data, (string, string, address, uint256));
+    function initToken(bytes calldata _data) external override {
+        (
+            string memory _name,
+            string memory _symbol,
+            address _owner,
+            uint256 _initialSupply
+        ) = abi.decode(_data, (string, string, address, uint256));
 
-        initToken(_name,_symbol,_owner,_initialSupply);
+        initToken(_name, _symbol, _owner, _initialSupply);
     }
 
-   /** 
-     * @dev Generates init data for Farm Factory
-     * @param _name - Token name
-     * @param _symbol - Token symbol
-     * @param _owner - Contract owner
-     * @param _initialSupply Amount of tokens minted on creation
-  */
     function getInitData(
         string calldata _name,
         string calldata _symbol,
@@ -57,8 +53,6 @@ contract FixedToken is ERC20, IMisoToken {
         pure
         returns (bytes memory _data)
     {
-        return abi.encode(_name, _symbol, _owner, _initialSupply);
+        _data = abi.encode(_name, _symbol, _owner, _initialSupply);
     }
-
-
 }
