695934207afa5e451ca18dba85aba0fdd5fd2ff3
Matthew Lilley <hello@matthewlilley.com>
2022-01-21 23:09:14 +0000

    chore: ignore task which imports typechain for now

--- tasks/add-all-tokens.ts
+++ tasks/add-all-tokens.ts
@@ -1,45 +1,38 @@
 import { task } from "hardhat/config";
 
-import {
-  FixedToken,
-  MintableToken,
-  MISOTokenFactory,
-  SushiToken,
-} from "../typechain";
+// import {
+//   FixedToken,
+//   MintableToken,
+//   MISOTokenFactory,
+//   SushiToken,
+// } from "../typechain";
 
 task("add-all-tokens", "Adds token").setAction(async function (
   _,
   { ethers: { getNamedSigner, getContract } }
 ) {
-  const admin = await getNamedSigner("admin");
-
-  const misoTokenFactory = await getContract<MISOTokenFactory>(
-    "MISOTokenFactory",
-    admin
-  );
-
-  const fixedToken = await getContract<FixedToken>("FixedToken", admin);
-
-  const mintableToken = await getContract<MintableToken>(
-    "MintableToken",
-    admin
-  );
-
-  const sushiToken = await getContract<SushiToken>("SushiToken", admin);
-
-  console.log("Adding tokens...");
-
-  await Promise.all([
-    await misoTokenFactory
-      .addTokenTemplate(fixedToken.address)
-      .then((tx) => tx.wait(3)),
-    await misoTokenFactory
-      .addTokenTemplate(mintableToken.address)
-      .then((tx) => tx.wait(3)),
-    await misoTokenFactory
-      .addTokenTemplate(sushiToken.address)
-      .then((tx) => tx.wait(3)),
-  ]);
-
-  console.log("Tokens added!");
+  // const admin = await getNamedSigner("admin");
+  // const misoTokenFactory = await getContract<MISOTokenFactory>(
+  //   "MISOTokenFactory",
+  //   admin
+  // );
+  // const fixedToken = await getContract<FixedToken>("FixedToken", admin);
+  // const mintableToken = await getContract<MintableToken>(
+  //   "MintableToken",
+  //   admin
+  // );
+  // const sushiToken = await getContract<SushiToken>("SushiToken", admin);
+  // console.log("Adding tokens...");
+  // await Promise.all([
+  //   await misoTokenFactory
+  //     .addTokenTemplate(fixedToken.address)
+  //     .then((tx) => tx.wait(3)),
+  //   await misoTokenFactory
+  //     .addTokenTemplate(mintableToken.address)
+  //     .then((tx) => tx.wait(3)),
+  //   await misoTokenFactory
+  //     .addTokenTemplate(sushiToken.address)
+  //     .then((tx) => tx.wait(3)),
+  // ]);
+  // console.log("Tokens added!");
 });
